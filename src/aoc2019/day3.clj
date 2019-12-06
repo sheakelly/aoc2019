3c8ea7ecd313ec900c96832a2ca72dd12327a30a
Shea Kelly <shea.kelly@lwb.org.au>
2019-12-06 16:58:06 +1100

    fns to draw paths

--- src/aoc2019/day3.clj
+++ src/aoc2019/day3.clj
@@ -2,9 +2,33 @@
   (:require [aoc2019.utils :as utils]
             [clojure.string :as str]))
 
-(->> (utils/get-input "day3.txt")
-  (str/split-lines)
-  (map #(str/split % #","))
-  (map (fn [x]
-         (map (fn [dir & cnt] [dir cnt]) x))))
+(defn wire-paths []
+  (->> (utils/get-input "day3.txt")
+    (str/split-lines)
+    (map #(str/split % #","))
+    (map (fn [x]
+           (map (fn [y] [(subs y 0 1) (read-string (subs y 1 (count y)))]) x)))))
 
+(defn plot-x [x y dist mod-fn]
+  (for [x' (range 1 (+ dist 1))] [(+ (mod-fn x') x) y]))
+
+(defn plot-y [x y dist mod-fn]
+  (for [y' (range 1 (+ dist 1))] [x (+ (mod-fn y') y)]))
+
+(plot-x 5 5 10 +)
+
+(defn draw-path [[dir dist] [x y]]
+  (println "dir" dir "dist" dist "x" x "y" y)
+  (cond
+    (= dir "L") (plot-x x y dist -)
+    (= dir "R") (plot-x x y dist +)
+    (= dir "U") (plot-y x y dist +)
+    (= dir "D") (plot-y x y dist -)))
+
+
+(draw-path ["R" 10] [5 5])
+(draw-path ["L" 10] [5 5])
+(draw-path ["U" 10] [5 5])
+(draw-path ["D" 8] [0 0])
+
+(wire-paths)
